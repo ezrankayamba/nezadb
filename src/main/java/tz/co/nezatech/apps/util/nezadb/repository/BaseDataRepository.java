1650d7e5f926cbaf6d0741ff420dd411b6992915
ezrankayamba <ezrankayamba@gmail.com>
2018-04-04 14:23:34 +0300

    1st commit for new branch "No spring Dependencies"

--- src/main/java/tz/co/nezatech/apps/util/nezadb/repository/BaseDataRepository.java
+++ src/main/java/tz/co/nezatech/apps/util/nezadb/repository/BaseDataRepository.java
@@ -1,90 +1,17 @@
 package tz.co.nezatech.apps.util.nezadb.repository;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 
 import tz.co.nezatech.apps.util.nezadb.model.Status;
 
 public abstract class BaseDataRepository<T> implements IDataRepository<T> {
-	private String orderBy = "";
-
-	public String getOrderBy() {
-		return orderBy;
-	}
-
-	public void setOrderBy(String orderBy) {
-		this.orderBy = orderBy;
-	}
-
-	@Override
-	public List<T> getAll() {
-		String sql = sqlFindAll() + (orderBy == null ? "" : orderBy);
-		return onList(getJdbcTemplate().query(sql, getRowMapper()));
-	}
-
-	public List<T> getAll(String column, Object value) {
-		String sql = sqlFindAll() + " and " + column + "='" + value + "'";
-		return onList(getJdbcTemplate().query(sql + (orderBy == null ? "" : orderBy), getRowMapper()));
-	}
-
-	public List<T> getAllNullable(String column, Boolean whereNull) {
-		String sql = sqlFindAll() + " and " + column + (whereNull ? " is null " : "is not null");
-		return onList(getJdbcTemplate().query(sql + (orderBy == null ? "" : orderBy), getRowMapper()));
-	}
-
-	@Override
-	public List<T> search(final String value) {
-		return onList(getJdbcTemplate().query(new PreparedStatementCreator() {
-
-			@Override
-			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-				return searchCriteria(value, conn);
-			}
-		}, getRowMapper()));
-	}
-	@Override
-	public List<T> search(final Map<String, Object> searchFileters) {
-		return onList(getJdbcTemplate().query(new PreparedStatementCreator() {
-
-			@Override
-			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-				return searchCriteria(searchFileters, conn);
-			}
-		}, getRowMapper()));
-	}
-	@Override
-	public List<T> search(final List<NamedQueryParam> params) {
-		return null;		
-	}
 
 	public Status create(final T entity) {
 		Status status = null;
 		try {
-			KeyHolder keyHolder = new GeneratedKeyHolder();
-			int res = getJdbcTemplate().update(new PreparedStatementCreator() {
-
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					return psCreate(entity, conn);
-				}
-
-			}, keyHolder);
-			int generatedId = keyHolder.getKey().intValue();
-			status = res == 1 ? new Status(200, "Successfully created enity " + entity, generatedId)
-					: new Status(500, "Failed to creat role " + entity + ". Error msg: No role created");
-		} catch (DataAccessException e) {
+			status= createEntity(entity);
+		} catch (Exception e) {
 			if (e.getMessage().contains("Duplicate entry") && updateOnDuplicate()) {
 				return this.update(entity);
 			} else {
@@ -101,53 +28,11 @@ public abstract class BaseDataRepository<T> implements IDataRepository<T> {
 	}
 
 	@Override
-	public T findById(long id) {
-		List<T> entities = onList(getJdbcTemplate().query(sqlFindById(), new Object[] { id }, getRowMapper()));
-		if (entities != null && !entities.isEmpty()) {
-			return entities.get(0);
-		}
-		return null;
-	}
-
-	@Override
-	public Status update(Integer id, final T entity) {
-		Status status = null;
-		try {
-			int res = getJdbcTemplate().update(new PreparedStatementCreator() {
-
-				@Override
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					return psUpdate(entity, conn);
-				}
-			});
-			status = res == 1 ? new Status(200, "Successfully updated entity " + entity)
-					: new Status(500, "Failed to update entity " + entity + ". Error msg: No entity updated");
-		} catch (DataAccessException e) {
-			e.printStackTrace();
-			return new Status(500, "Failed to update entity " + entity + ". Error msg: " + e.getMessage());
-		}
-
-		if (status.getCode() == 200) {
-			return onSave(entity, status);
-		} else {
-			return status;
-		}
-	}
-
-	@Override
 	public Status update(final T entity) {
 		Status status = null;
 		try {
-			int res = getJdbcTemplate().update(new PreparedStatementCreator() {
-
-				@Override
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					return psUpdateByKey(entity, conn);
-				}
-			});
-			status = res == 1 ? new Status(200, "Successfully updated entity " + entity)
-					: new Status(500, "Failed to update entity " + entity + ". Error msg: No entity updated");
-		} catch (DataAccessException e) {
+			status = updateEntity(entity);
+		} catch (Exception e) {
 			e.printStackTrace();
 			return new Status(500, "Failed to update entity " + entity + ". Error msg: " + e.getMessage());
 		}
@@ -164,93 +49,5 @@ public abstract class BaseDataRepository<T> implements IDataRepository<T> {
 		PreparedStatement ps = psUpdate(entity, conn);
 		return ps;
 	}
-
-	@Override
-	public Status delete(final long id) {
-		try {
-			int res = getJdbcTemplate().update(new PreparedStatementCreator() {
-
-				@Override
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					return psDelete(id, conn);
-				}
-			});
-			return res == 1 ? new Status(200, "Successfully deleted entity " + id)
-					: new Status(500, "Failed to delete entity " + id + ". Error msg: No entity deleted");
-		} catch (DataAccessException e) {
-			e.printStackTrace();
-			return new Status(500, "Failed to delete entity " + id + ". Error msg: " + e.getMessage());
-		}
-	}
 	
-	@Override
-	public Status deleteLinked(final long id) {
-		try {
-			int res = getJdbcTemplate().update(new PreparedStatementCreator() {
-
-				@Override
-				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-					return psDeleteLinked(id, conn);
-				}
-			});
-			return res >= 1 ? new Status(200, "Successfully deleted entities linked to " + id)
-					: new Status(500, "Failed to delete entities linked to " + id + ". Error msg: No entity deleted");
-		} catch (DataAccessException e) {
-			e.printStackTrace();
-			return new Status(500, "Failed to delete entities linked to " + id + ". Error msg: " + e.getMessage());
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public T schema() {
-		Type type = getClass().getGenericSuperclass();
-		ParameterizedType paramType = (ParameterizedType) type;
-		Class<T> cls = (Class<T>) (paramType.getActualTypeArguments()[0]);
-		try {
-			return cls.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	@Override
-	public boolean updateOnDuplicate() {
-		return false;
-	}
-
-	@Override
-	public List<T> onList(List<T> list) {
-		return list;
-	}
-
-	@Override
-	public Timestamp toSQLTimestamp(Date date) {
-		Timestamp tsp = null;
-		if (date != null)
-			tsp = new Timestamp(date.getTime());
-		return tsp;
-	}
-
-	@Override
-	public Date fromSQLTimestamp(Timestamp timestamp) {
-		Date date = null;
-		if (timestamp != null)
-			date = new Date(timestamp.getTime());
-		return date;
-	}
-	
-	
-
-	@Override
-	public PreparedStatement searchCriteria(Map<String, Object> searchFileters, Connection conn) {
-		throw new UnsupportedOperationException("NamedParameterJdbcTemplate Not supported by this repository. Consider implementing this");
-	}
-
-	@Override
-	public NamedParameterJdbcTemplate getNamedParamsJdbcTemplate() {
-		throw new UnsupportedOperationException("NamedParameterJdbcTemplate Not supported by this repository. Consider implementing this");
-	}
 }
--- src/main/java/tz/co/nezatech/apps/util/nezadb/repository/IDataRepository.java
+++ src/main/java/tz/co/nezatech/apps/util/nezadb/repository/IDataRepository.java
@@ -1,5 +1,7 @@
 package tz.co.nezatech.apps.util.nezadb.repository;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Timestamp;
@@ -7,42 +9,140 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
 import tz.co.nezatech.apps.util.nezadb.model.Status;
 
 public interface IDataRepository<T extends Object> {
-	public RowMapper<T> getRowMapper();
+	/**
+	 * 
+	 * @param sql
+	 * @return a list of entities T
+	 * 
+	 *         {@code List<T> queryEntinties(String sql) {return
+	 *         getJdbcTemplate().query(sql, getRowMapper()); } }
+	 * 
+	 * 
+	 */
+	public List<T> queryEntinties(String sql);
+
+	/**
+	 * 
+	 * @param value
+	 * @return a list of entities
+	 * 
+	 *         {@code List<T> searchEntities(final String value) {
+		return getJdbcTemplate().query(new PreparedStatementCreator() {
+	 * 
+	 * 		@Override public PreparedStatement createPreparedStatement(Connection
+	 *         conn) throws SQLException { return searchCriteria(value, conn); } },
+	 *         getRowMapper()); }}
+	 * 
+	 */
+	public List<T> searchEntities(final String value);
+
+	/**
+	 * 
+	 * @param searchFileters
+	 * @return
+	 * 
+	 * 		{@code List<T> searchEntinties(final Map<String, Object> searchFileters) {
+		return getJdbcTemplate().query(new PreparedStatementCreator() {
+	 * 
+	 * 		@Override public PreparedStatement createPreparedStatement(Connection
+	 *         conn) throws SQLException { return searchCriteria(searchFileters,
+	 *         conn); } }, getRowMapper()); }}
+	 */
+	public List<T> searchEntinties(final Map<String, Object> searchFileters);
+
+	/**
+	 * 
+	 * @param entity
+	 * @return
+	 * 
+	 * 		{@code public Status createEntity(final T entity) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		int res = getJdbcTemplate().update(new PreparedStatementCreator() {
+	
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException
+	 *         { return psCreate(entity, conn); }
+	 * 
+	 *         }, keyHolder); int generatedId = keyHolder.getKey().intValue();
+	 *         Status status = res == 1 ? new Status(200, "Successfully created
+	 *         enity " + entity, generatedId) : new Status(500, "Failed to creat
+	 *         role " + entity + ". Error msg: No role created"); return status; }}
+	 */
+	public Status createEntity(final T entity);
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * 
+	 * 		{@code public T findEntityById(long id) { List<T> entities =
+	 *         onList(getJdbcTemplate().query(sqlFindById(), new Object[] { id },
+	 *         getRowMapper())); if (entities != null && !entities.isEmpty()) {
+	 *         return entities.get(0); } return null; }}
+	 */
+	public T findEntityById(long id);
+
+	/**
+	 * 
+	 * @param entity
+	 * @return
+	 * 
+	 * 
+	 * 		{@code public Status updateEntity(final T entity) {
+		Status status;
+		int res = getJdbcTemplate().update(new PreparedStatementCreator() {
+	 * 
+	 * 		@Override public PreparedStatement createPreparedStatement(Connection
+	 *         conn) throws SQLException { return psUpdateByKey(entity, conn); } });
+	 *         status = res == 1 ? new Status(200, "Successfully updated entity " +
+	 *         entity) : new Status(500, "Failed to update entity " + entity + ".
+	 *         Error msg: No entity updated"); return status; }}
+	 */
+	public Status updateEntity(final T entity);
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * 
+	 * 		{@code public Status delete(final long id) {
+		int res = getJdbcTemplate().update(new PreparedStatementCreator() {
+	 * 
+	 * 		@Override public PreparedStatement createPreparedStatement(Connection
+	 *         conn) throws SQLException { return psDelete(id, conn); } }); return
+	 *         res == 1 ? new Status(200, "Successfully deleted entity " + id) : new
+	 *         Status(500, "Failed to delete entity " + id + ". Error msg: No entity
+	 *         deleted"); }}
+	 */
+	public Status delete(final long id);
+
+	/**
+	 * 
+	 * @return
+	 * 
+	 * 
+	 * 		{@code public  Status deleteLinked(final long id) {
+		int res = getJdbcTemplate().update(new PreparedStatementCreator() {
+	 * 
+	 * 		@Override public PreparedStatement createPreparedStatement(Connection
+	 *         conn) throws SQLException { return psDeleteLinked(id, conn); } });
+	 *         return res >= 1 ? new Status(200, "Successfully deleted entities
+	 *         linked to " + id) : new Status(500, "Failed to delete entities linked
+	 *         to " + id + ". Error msg: No entity deleted"); }}
+	 */
+	public Status deleteLinked(final long id);
 
 	public String getOrderBy();
 
-	public void setOrderBy(String orderBy);
-
-	public List<T> getAll();
-
-	public List<T> getAll(String column, Object value);
-
-	public List<T> search(String value);
-	public List<T> search(Map<String, Object> searchFileters);
-
-	public PreparedStatement searchCriteria(String value, Connection conn);
 	public PreparedStatement searchCriteria(Map<String, Object> searchFileters, Connection conn);
 
-	public List<T> getAllNullable(String column, Boolean whereNull);
-
 	public Status create(final T entity);
-
-	public T findById(long id);
 
 	public Status update(Integer id, T entity);
 
 	public Status update(T entity);
-
-	public Status delete(long id);
-
-	public Status deleteLinked(long id);
 
 	public String sqlFindAll();
 
@@ -58,21 +158,69 @@ public interface IDataRepository<T extends Object> {
 
 	public PreparedStatement psDeleteLinked(long id, Connection conn);
 
-	public JdbcTemplate getJdbcTemplate();
+	default public T findById(long id) {
+		return findEntityById(id);
+	}
 
-	public NamedParameterJdbcTemplate getNamedParamsJdbcTemplate();
+	@SuppressWarnings("unchecked")
+	default public T schema() {
+		Type type = getClass().getGenericSuperclass();
+		ParameterizedType paramType = (ParameterizedType) type;
+		Class<T> cls = (Class<T>) (paramType.getActualTypeArguments()[0]);
+		try {
+			return cls.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
 
-	public T schema();
+		return null;
+	}
 
 	public boolean updateOnDuplicate();
 
 	public Status onSave(T entity, Status status);
 
-	public List<T> onList(List<T> list);
+	default public List<T> onList(List<T> list) {
+		return list;
+	}
 
-	public Date fromSQLTimestamp(Timestamp timestamp);
+	default public Timestamp toSQLTimestamp(Date date) {
+		Timestamp tsp = null;
+		if (date != null)
+			tsp = new Timestamp(date.getTime());
+		return tsp;
+	}
 
-	public Timestamp toSQLTimestamp(Date date);
+	default public Date fromSQLTimestamp(Timestamp timestamp) {
+		Date date = null;
+		if (timestamp != null)
+			date = new Date(timestamp.getTime());
+		return date;
+	}
 
-	List<T> search(List<NamedQueryParam> params);
+	default public List<T> getAll() {
+
+		String sql = sqlFindAll() + (getOrderBy() == null ? "" : getOrderBy());
+		return onList(queryEntinties(sql));
+	}
+
+	default public List<T> getAll(String column, Object value) {
+		String sql = sqlFindAll() + " and " + column + "='" + value + "'";
+		sql += getOrderBy() == null ? " " : getOrderBy();
+		return onList(queryEntinties(sql));
+	}
+
+	default public List<T> getAllNullable(String column, Boolean whereNull) {
+		String sql = sqlFindAll() + " and " + column + (whereNull ? " is null " : "is not null");
+		sql += getOrderBy() == null ? " " : getOrderBy();
+		return onList(queryEntinties(sql));
+	}
+
+	default public List<T> search(final String value) {
+		return onList(searchEntities(value));
+	}
+
+	default public List<T> search(final Map<String, Object> searchFileters) {
+		return onList(searchEntinties(searchFileters));
+	}
 }
